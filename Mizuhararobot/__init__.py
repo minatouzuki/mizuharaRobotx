18350a68a226ebee2c77870b5876f0c6c0874b9d
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2020-12-07 20:43:44 +0530

    Update __init__.py

--- Mizuhararobot/__init__.py
+++ Mizuhararobot/__init__.py
@@ -3,10 +3,9 @@ import os
 import sys
 import time
 import spamwatch
+
 import telegram.ext as tg
 from telethon import TelegramClient
-from redis import StrictRedis
-from pyrogram import Client, errors
 
 StartTime = time.time()
 

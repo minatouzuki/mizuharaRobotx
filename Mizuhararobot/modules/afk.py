c8cabcf008dd2bd02a3058b8cd5c069259224934
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2020-12-07 18:08:14 +0530

    Update afk.py

--- Mizuhararobot/modules/afk.py
+++ Mizuhararobot/modules/afk.py
@@ -57,7 +57,7 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
+                '{} is here!', '{} is back with a bang!', '{} is now in the chat after all things over!',
                 '{} is awake!', '{} is back online!', '{} is finally here!',
                 'Welcome back! {}', 'Where is {}?\nIn the chat!'
             ]

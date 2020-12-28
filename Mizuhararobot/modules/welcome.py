f40a533220d9b3f13af56450228837f12da322c2
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2020-12-28 15:21:23 +0530

    Update welcome.py

--- Mizuhararobot/modules/welcome.py
+++ Mizuhararobot/modules/welcome.py
@@ -181,7 +181,7 @@ def new_member(update: Update, context: CallbackContext):
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "Oh, Genos? Let's get this moving.", reply_to_message_id=reply
+                    "Oh kami-san,command me.", reply_to_message_id=reply
                 )
                 welcome_log = (
                     f"{html.escape(chat.title)}\n"
@@ -193,7 +193,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Devs
             elif new_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "Whoa! A member of the Heroes Association just joined!",
+                    "Whoa! A member of the Rental Association just joined!",
                     reply_to_message_id=reply,
                 )
                 continue
@@ -201,7 +201,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Sudos
             elif new_mem.id in DRAGONS:
                 update.effective_message.reply_text(
-                    "Huh! A Dragon disaster just joined! Stay Alert!",
+                    "Huh! A Tsar disaster just joined! Stay Alert!",
                     reply_to_message_id=reply,
                 )
                 continue
@@ -209,7 +209,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Support
             elif new_mem.id in DEMONS:
                 update.effective_message.reply_text(
-                    "Huh! Someone with a Demon disaster level just joined!",
+                    "Huh! Someone with a Hussar disaster level just joined!",
                     reply_to_message_id=reply,
                 )
                 continue
@@ -217,14 +217,14 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Whitelisted
             elif new_mem.id in TIGERS:
                 update.effective_message.reply_text(
-                    "Oof! A Tiger disaster just joined!", reply_to_message_id=reply
+                    "Oof! A Ronin disaster just joined!", reply_to_message_id=reply
                 )
                 continue
 
             # Welcome Tigers
             elif new_mem.id in WOLVES:
                 update.effective_message.reply_text(
-                    "Oof! A Wolf disaster just joined!", reply_to_message_id=reply
+                    "Oof! A Envoy disaster just joined!", reply_to_message_id=reply
                 )
                 continue
 

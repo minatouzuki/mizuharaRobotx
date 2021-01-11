65237729ffbd0b035bc5d3d88d62b3f2a3c55f2e
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-12 04:27:39 +0530

    Update __main__.py

--- Mizuhararobot/__main__.py
+++ Mizuhararobot/__main__.py
@@ -37,7 +37,7 @@ from Mizuhararobot.modules.helper_funcs.alternate import typing_action
 
 
 PM_START_TEXT = f"""
-Hello there,The name's 
+Hello there,The name is *{dispatcher.bot.first_name}* [ʕ·ᴥ·ʔ](https://telegra.ph/file/8fbc5858ddd7eefe2fccf.jpg)
 
 I am an Anime Themed Group Managing Bot and I will help in managing your group
 

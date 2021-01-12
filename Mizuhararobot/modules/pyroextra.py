7b42a37221b62053834938250a07adcb8005f480
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-12 19:21:59 +0530

    Create pyroextra.py

---
+++ Mizuhararobot/modules/pyroextra.py
@@ -0,0 +1,112 @@
+import re
+import html
+import regex
+import aiohttp
+from datetime import datetime
+from asyncio import sleep
+import os
+from pytube import YouTube
+from youtubesearchpython import VideosSearch
+from Mizuhararobot.utils.ut import get_arg
+from Mizuhararobot import pbot, LOGGER
+from pyrogram import Client, filters
+from pyrogram.errors import PeerIdInvalid
+from pyrogram.types import Message
+
+
+@pbot.on_message(filters.command("song"))
+async def song(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+    args = get_arg(message) + " " + "song"
+    if args.startswith(" "):
+        await message.reply("Enter a song name. Check /help")
+        return ""
+    status = await message.reply("Processing...")
+    video_link = yt_search(args)
+    if not video_link:
+        await status.edit("Song not found.")
+        return ""
+    yt = YouTube(video_link)
+    audio = yt.streams.filter(only_audio=True).first()
+    try:
+        download = audio.download(filename=f"{str(user_id)}")
+    except Exception as ex:
+        await status.edit("Failed to download song")
+        LOGGER.error(ex)
+        return ""
+    rename = os.rename(download, f"{str(user_id)}.mp3")
+    await pbot.send_chat_action(message.chat.id, "upload_audio")
+    await pbot.send_audio(
+        chat_id=message.chat.id,
+        audio=f"{str(user_id)}.mp3",
+        duration=int(yt.length),
+        title=str(yt.title),
+        performer=str(yt.author),
+        reply_to_message_id=message.message_id,
+    )
+    await status.delete()
+    os.remove(f"{str(user_id)}.mp3")
+
+
+@pbot.on_message(filters.command("spbinfo"))
+async def lookup(client, message):
+    cmd = message.command
+    if not message.reply_to_message and len(cmd) == 1:
+        get_user = message.from_user.id
+    elif len(cmd) == 1:
+        if message.reply_to_message.forward_from:
+            get_user = message.reply_to_message.forward_from.id
+        else:
+            get_user = message.reply_to_message.from_user.id
+    elif len(cmd) > 1:
+        get_user = cmd[1]
+        try:
+            get_user = int(cmd[1])
+        except ValueError:
+            pass
+    try:
+        user = await client.get_chat(get_user)
+    except PeerIdInvalid:
+        await message.reply_text("I don't know that User.")
+        sleep(2)
+        return
+    url = f"https://api.intellivoid.net/spamprotection/v1/lookup?query={user.id}"
+    a = await AioHttp().get_json(url)
+    response = a["success"]
+    if response == True:
+        date = a["results"]["last_updated"]
+        stats = f"**◢ Intellivoid• SpamProtection Info**:\n"
+        stats += f' • **Updated on**: `{datetime.fromtimestamp(date).strftime("%Y-%m-%d %I:%M:%S %p")}`\n'
+        stats += (
+            f" • **Chat Info**: [Link](t.me/SpamProtectionBot/?start=00_{user.id})\n"
+        )
+
+        if a["results"]["attributes"]["is_potential_spammer"] == True:
+            stats += f" • **User**: `USERxSPAM`\n"
+        elif a["results"]["attributes"]["is_operator"] == True:
+            stats += f" • **User**: `USERxOPERATOR`\n"
+        elif a["results"]["attributes"]["is_agent"] == True:
+            stats += f" • **User**: `USERxAGENT`\n"
+        elif a["results"]["attributes"]["is_whitelisted"] == True:
+            stats += f" • **User**: `USERxWHITELISTED`\n"
+
+        stats += f' • **Type**: `{a["results"]["entity_type"]}`\n'
+        stats += (
+            f' • **Language**: `{a["results"]["language_prediction"]["language"]}`\n'
+        )
+        stats += f' • **Language Probability**: `{a["results"]["language_prediction"]["probability"]}`\n'
+        stats += f"**Spam Prediction**:\n"
+        stats += f' • **Ham Prediction**: `{a["results"]["spam_prediction"]["ham_prediction"]}`\n'
+        stats += f' • **Spam Prediction**: `{a["results"]["spam_prediction"]["spam_prediction"]}`\n'
+        stats += f'**Blacklisted**: `{a["results"]["attributes"]["is_blacklisted"]}`\n'
+        if a["results"]["attributes"]["is_blacklisted"] == True:
+            stats += (
+                f' • **Reason**: `{a["results"]["attributes"]["blacklist_reason"]}`\n'
+            )
+            stats += f' • **Flag**: `{a["results"]["attributes"]["blacklist_flag"]}`\n'
+        stats += f'**PTID**:\n`{a["results"]["private_telegram_id"]}`\n'
+        await message.reply_text(stats, disable_web_page_preview=True)
+    else:
+        await message.reply_text("`cannot reach SpamProtection API`")
+        await sleep(3)

bc63b60e4e29b17b1810626650eebc312530eeea
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-12 21:15:42 +0530

    Update userinfo.py

--- Mizuhararobot/modules/userinfo.py
+++ Mizuhararobot/modules/userinfo.py
@@ -549,7 +549,7 @@ Examples:
  • `/info`*:* get information about a user. 
  
 *What is that health thingy?*
- Come and see [HP System explained](https://t.me/OnePunchUpdates/192)
+ Come and see [HP System explained](https://t.me/chizuruupdates/21)
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)

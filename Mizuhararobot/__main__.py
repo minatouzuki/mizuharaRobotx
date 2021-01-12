5dd515d40760f067b04bb8a12e2875106a4b69a4
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-12 15:04:45 +0530

    Update __main__.py

--- Mizuhararobot/__main__.py
+++ Mizuhararobot/__main__.py
@@ -34,13 +34,12 @@ BOT_IMG = "https://telegra.ph/file/0f6a2472ca83a579bcff3.mp4"
 
 
 HELP_STRINGS = """
-Hello I'm  mizuhara a rent girlfriend rent me for your grp to have fun there.You can have a look at the following for an idea of some of \
+Hello I'm  mizuhara from rent a girlfriend. Rent me for your grp to have fun there.You can have a look at the following for an idea of some of \
 the things I can help you with.
 
 *Main* commands available:[:.:](https://telegra.ph/file/8fbc5858ddd7eefe2fccf.jpg)
  • /help: PM's you this message.
  • /help <module name>: PM's you info about that module.
- • /donate: information on how to donate!
  • /settings:
    • in PM: will send you your settings for all supported modules.
    • in a group: will redirect you to pm, with all that chat's settings.

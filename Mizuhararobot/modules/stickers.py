0cef9d1e5ff5c466c3c53780053963aa9ce4e4f7
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-02-04 18:11:38 +0530

    Update stickers.py (#9)

--- Mizuhararobot/modules/stickers.py
+++ Mizuhararobot/modules/stickers.py
@@ -662,22 +662,6 @@ def remove_fvrtsticker(update, context):
             parse_mode=ParseMode.HTML
         )
         
-__help__ = """
-Stickers made easy with stickers module!
-
- • `/stickers`: Find stickers for given term on combot sticker catalogue 
- • `/kang`: Reply to a sticker to add it to your pack.
- • `/remove`: Reply to your anime exist sticker to your pack to delete it.
- • `/stickerid`: Reply to a sticker to me to tell you its file ID.
- • `/getsticker`: Reply to a sticker to me to upload its raw PNG file.
- • `/addfav or /afv <custom name>`: Reply to a sticker to add it into your favorite pack list.
- • `/myfav or /mfv`: Get list of your favorite packs.
- • `/removefav or /rfv <custom name>|: Reply to a sticker to remove it into your favorite pack list.
-
-*Example:* `/addfav mizuhara`
-"""
-
-__mod_name__ = "Stickers"
 
 KANG_HANDLER = DisableAbleCommandHandler("kang" , addsticker, pass_args=True)
 DEL_HANDLER = DisableAbleCommandHandler("remove", delsticker)

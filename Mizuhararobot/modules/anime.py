44da34f8c9df7e4ec3c6817483676ccb3f6cfe67
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-02-10 13:48:17 +0530

    Update anime.py

--- Mizuhararobot/modules/anime.py
+++ Mizuhararobot/modules/anime.py
@@ -721,11 +721,11 @@ def site_search(update: Update, context: CallbackContext, site: str):
         soup = bs4.BeautifulSoup(html_text, "html.parser")
         search_result = soup.find_all("h2", {"class": "title"})
 
-        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>AnimeKayo</code>: \n"
+        result = f"<b>Search results for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>AnimePahe</code>: \n"
         for entry in search_result:
 
             if entry.text.strip() == "Nothing Found":
-                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>AnimeKayo</code>"
+                result = f"<b>No result found for</b> <code>{html.escape(search_query)}</code> <b>on</b> <code>AnimePahe</code>"
                 more_results = False
                 break
 

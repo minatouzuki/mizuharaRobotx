fc7858bf4a6adf63e89fdbe4c4eb0b2bb07d14cb
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-02-10 15:45:16 +0530

    Update global_bans.py

--- Mizuhararobot/modules/global_bans.py
+++ Mizuhararobot/modules/global_bans.py
@@ -584,7 +584,7 @@ dispatcher.add_handler(UNGBAN_HANDLER)
 dispatcher.add_handler(GBAN_LIST)
 dispatcher.add_handler(GBAN_STATUS)
 
-__mod_name__ = "Global Bans"
+__mod_name__ = "AntiSpam"
 __handlers__ = [GBAN_HANDLER, UNGBAN_HANDLER, GBAN_LIST, GBAN_STATUS]
 
 if STRICT_GBAN:  # enforce GBANS if this is set

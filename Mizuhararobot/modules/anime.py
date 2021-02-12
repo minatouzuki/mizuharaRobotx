b55c59ad20f3caeb5ab193c3342054bf6883c792
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-02-12 17:27:30 +0530

    Update anime.py

--- Mizuhararobot/modules/anime.py
+++ Mizuhararobot/modules/anime.py
@@ -193,11 +193,8 @@ def anime(update, context):
     else:
         search = search[1]
     variables = {'search': search}
-    json = requests.post(
-        url, json={
-            'query': anime_query,
-            'variables': variables
-        }).json()
+    json = requests.post(url, json={'query': anime_query, 'variables': variables}).json()[
+        'data'].get('Media', None)
     if 'errors' in json.keys():
         update.effective_message.reply_text('Anime not found')
         return

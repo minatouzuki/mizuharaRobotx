5932d614e9a8469c5b9fa7dde2019fecde2b1cd4
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-05-09 23:11:51 +0530

    Added nhentai search

--- Mizuhararobot/modules/anime.py
+++ Mizuhararobot/modules/anime.py
@@ -5,7 +5,7 @@ import bs4
 import html
 from inspect import getfullargspec
 
-from Mizuhararobot import pbot
+from Mizuhararobot import pbot, telegraph 
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Update
@@ -316,6 +316,72 @@ async def upcoming(c: Client, m: Message):
     await m.reply_text(upcoming_message)
 
 
+@pbot.on_message(filters.command("nhentai"))
+async def nhentai(_, message):
+    if len(message.command) < 2:
+        await message.delete()
+        return
+    query = message.text.split(None, 1)[1]
+    title, tags, artist, total_pages, post_url, cover_image = nhentai_data(
+        query)
+    await message.reply_text(
+        f"<code>{title}</code>\n\n<b>Tags:</b>\n{tags}\n<b>Artists:</b>\n{artist}\n<b>Pages:</b>\n{total_pages}",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Read Here",
+                        url=post_url
+                    )
+                ]
+            ]
+        )
+    )
+
+
+def nhentai_data(noombers):
+    url = f"https://nhentai.net/api/gallery/{noombers}"
+    res = requests.get(url).json()
+    pages = res["images"]["pages"]
+    info = res["tags"]
+    title = res["title"]["english"]
+    links = []
+    tags = ""
+    artist = ''
+    total_pages = res['num_pages']
+    extensions = {
+        'j': 'jpg',
+        'p': 'png',
+        'g': 'gif'
+    }
+    for i, x in enumerate(pages):
+        media_id = res["media_id"]
+        temp = x['t']
+        file = f"{i+1}.{extensions[temp]}"
+        link = f"https://i.nhentai.net/galleries/{media_id}/{file}"
+        links.append(link)
+
+    for i in info:
+        if i["type"] == "tag":
+            tag = i['name']
+            tag = tag.split(" ")
+            tag = "_".join(tag)
+            tags += f"#{tag} "
+        if i["type"] == "artist":
+            artist = f"{i['name']} "
+
+    post_content = "".join(f"<img src={link}><br>" for link in links)
+
+    post = telegraph.create_page(
+        f"{title}",
+        html_content=post_content,
+        author_name="@Chizurumanagementbot",
+        author_url="https://t.me/Chizurumanagementbot"
+    )
+    return title, tags, artist, total_pages, post['url'], links[0]
+
+
+
 __help__ = """
 
 Get information about anime, manga or characters from [AniList](anilist.co) and [MyAnimeList](myanimelist.net).
@@ -329,6 +395,7 @@ Get information about anime, manga or characters from [AniList](anilist.co) and 
  ~ `/upcoming`: returns a list of new anime in the upcoming seasons from Anilist.
  ~ `/supcoming`: returns a list of new anime in the upcoming seasons from MyAnimeList.
  ~ `/airing` <anime>: returns anime airing info.
+ ~ `/nhentai` <ID>: returns information from nhentai in telegraph page.
  ~ `/kaizoku` <anime>: search an anime on animekaizoku.com
  ~ `/kayo` <anime>: search an anime on animekayo.com
  """

a0dba51428687d48e32892bc6f5d9cdcbdb57556
stormshadow3000 <83730643+stormshadow3000@users.noreply.github.com>
2021-05-11 00:06:16 +0530

    Update sample_config.py

--- Mizuhararobot/sample_config.py
+++ Mizuhararobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/Mizuhararobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Jin mori/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
